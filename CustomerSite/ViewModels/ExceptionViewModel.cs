2963aea2cf618f5f0fc3d24e4b40e95ca5419480
kawsar-hamid-git <kawsar.hamid.git@gmail.com>
2024-04-08 06:14:39 +0600

    saas accelarator project code

---
+++ CustomerSite/ViewModels/ExceptionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Marketplace.SaaS.Accelerator.CustomerSite.ViewModels;
+
+public class ExceptionViewModel
+{
+    public bool ShowExceptionDetailForDeveloper { get; set; }
+    public Exception Exception { get; set; }
+}
